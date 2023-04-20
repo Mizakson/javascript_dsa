144193ef97f5be7c62ff8f1f0497058685e1630e
Mizakson <maxwellizakson@gmail.com>
2023-04-20 09:55:36 -0400

    created assignment_operator.js

---
+++ basics/assignment_operator.js
@@ -0,0 +1,17 @@
+/* 
+assignment_operator
+
+you can store a value in a variable using the assignment operator (=).
+here is an example usage...
+
+first declare a variable:
+var myVar;
+
+then assign a value to it:
+myVar = 5;
+
+*/
+
+// another example declaring a variable (a) and assigning it the value 7
+var a;
+a = 7;
