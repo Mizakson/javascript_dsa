47d1ce9bfa8f11a3d70657aaacc85fa95c8ea073
Mizakson <maxwellizakson@gmail.com>
2023-04-20 10:11:58 -0400

    modified variables.js

--- basics/variables.js
+++ basics/variables.js
@@ -1,5 +1,7 @@
 /* 
+
 there are 8 different data types in javascript - will go further in-depth later
+
 1. undefined
 2. null
 3. boolean
@@ -8,7 +10,33 @@ there are 8 different data types in javascript - will go further in-depth later
 6. bigint
 7. number
 8. object
+
 */
 
 // here is how to declare a variable called myName
 var myName;
+
+/* 
+
+to declare a string variable 
+double quotation marks ""
+need to surround the string after the assignment operator
+
+*/
+
+// here is an example declaring two variables, myFirstName and myLastName
+var myFirstName = "John";
+var myLastName = "Doe";
+
+
+/* 
+uninitialized variables:
+
+when variables are declared they have an initial value of undefined.
+
+if you do a mathematical operation on an undefined variable you get the result NaN (Not a Number).
+
+if you concatenate a string with an undefined variable, you will get a string of undefined.
+
+*/
+
