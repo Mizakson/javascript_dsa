80f99c8cd62bf2b7c104eb8b6a264adeded97b07
Mizakson <maxwellizakson@gmail.com>
2023-04-20 09:49:57 -0400

    created variables.js

---
+++ basics/variables.js
@@ -0,0 +1,14 @@
+/* 
+there are 8 different data types in javascript - will go further in-depth later
+1. undefined
+2. null
+3. boolean
+4. string
+5. symbol
+6. bigint
+7. number
+8. object
+*/
+
+// here is how to declare a variable called myName
+var myName;
