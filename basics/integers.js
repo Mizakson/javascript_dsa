c8d78135a01b8b3d40b59e99861d6ccd95024097
Mizakson <maxwellizakson@gmail.com>
2023-04-20 13:23:22 -0400

    modified integers.js

---
+++ basics/integers.js
@@ -0,0 +1,121 @@
+//integer operations in JavaScript
+
+// addition +
+const myVar = 2 + 2;
+
+//subtraction -
+const myVar2 = 55 - 10;
+
+//multiplication *
+const myVar3 = 7 * 8;
+
+//division /
+const myVar4 = 88 / 11;
+
+
+/* 
+increment (add 1) to a number 
+
+put ++ at the end of the variable.
+
+ex. 
+i++;
+
+which is equivalent to
+
+i = i + 1;
+
+*/
+
+/* 
+decrement (minus 1) to a number 
+
+put -- at the end of the variable.
+
+ex. 
+i--;
+
+which is equivalent to
+
+i = i - 1;
+
+*/
+
+
+/* 
+difference between an integer and a float:
+
+a float is a number that has a decimal point.
+
+float = 2.5
+int = 3
+
+ex.
+myDecimal = 5.7;
+
+*/
+
+
+/* 
+remainder operator (%):
+gives the remainder of the division of two numbers
+
+ex. 
+5 % 2 = 1
+% / 2 = 2 remainder = 1
+2 * 2 = 4
+5 - 4 = 1
+
+the remainder operator is used to check if a number is even or odd.
+even numbers have a remainder of 0 and odd numbers have a remainder of 1
+
+***
+remainder is not the same as the modulus operator, 
+it is similar but doesn't work correctly with negative numbers
+***
+
+*/
+
+/* 
+compound assignments:
+operators that can do a mathematical operation and assignment in one step
+
+for addition the operator is +=
+also referred to as augmented addition
+
+ex.
+myVar = myVar + 5;
+or you can use the += operator
+let myVar = 1;
+myVar += 5; (the same as myVar = myVar + 5)
+
+
+for subtraction the operator is -=
+also referred to as augmented subtraction
+
+ex.
+myVar = myVar - 5;
+or you can use the -= operator
+myVar -= 5;
+
+
+for multiplication the operator is *=
+also referred to as augmented multiplication
+
+ex.
+myVar = myVar * 5;
+or you can use the *= operator;
+myVar *= 5;
+
+
+for division the operator is /=
+also referred to as augmenred division
+
+ex.
+myVar = myVar / 5;
+or you can use the /= operator
+myVar /= 5;
+
+
+*/
+
