dea6186ce82d53a01088d2fa5a33c57310f0a29b
Mizakson <maxwellizakson@gmail.com>
2023-04-20 10:13:09 -0400

    modified comments.js

--- basics/comments.js
+++ basics/comments.js
@@ -1,5 +1,6 @@
 // this is how you comment a single line in JavaScript
 
-/* this is how you comment multiple lines in JavaScript 
+/* 
+this is how you comment multiple lines in JavaScript 
 here is another line of text
 */
