545c02b421f65689dc8014beeffbe4986cae5665
Mizakson <maxwellizakson@gmail.com>
2023-04-21 15:21:09 -0400

    modified shopping_list.js

--- module_1/notes/shopping_list.js
+++ module_1/notes/shopping_list.js
@@ -1,3 +1,12 @@
 // shopping list 
 // array practice
 
+// ["item_name" (str), item_quantity (int)]
+const myList = [
+    ["Apple", 4],
+    ["Orange", 6],
+    ["Carrot", 2],
+    ["Ice Cream", 2],
+    ["Lemon", 2],
+    ["Tea", 2]
+];
