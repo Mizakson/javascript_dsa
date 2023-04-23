99b0ac83530b600580fbca0824b995f1abedf6d6
Mizakson <maxwellizakson@gmail.com>
2023-04-23 12:52:16 -0400

    modified control_flow.js

--- module_1/notes/sub_module_2/control_flow.js
+++ module_1/notes/sub_module_2/control_flow.js
@@ -1,6 +1,22 @@
 // notes on conditional logic + control flow
 
 /* 
+if statements:
 
+used to make decisions in code.
+
+if(condition is true) {
+    statement is executed
+}
+
+function test (myCondition) {
+    if (myCondition) {
+        return "It was true";
+    }
+    return "It was false";
+}
+
+test(true); // output = It was true
+test(false); // output = It was false
 
 */
