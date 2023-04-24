95f6f1fcbc6e68beb0631109183d97b1705c2904
Mizakson <maxwellizakson@gmail.com>
2023-04-24 14:05:59 -0400

    created record_collection.js

---
+++ module_1/notes/sub_module_4/record_collection.js
@@ -0,0 +1,31 @@
+// sub module 4 final mini program
+// record collection
+
+// base record collection
+const recordCollection = {
+    2548: {
+      albumTitle: 'Slippery When Wet',
+      artist: 'Bon Jovi',
+      tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    },
+    2468: {
+      albumTitle: '1999',
+      artist: 'Prince',
+      tracks: ['1999', 'Little Red Corvette']
+    },
+    1245: {
+      artist: 'Robert Palmer',
+      tracks: []
+    },
+    5439: {
+      albumTitle: 'ABBA Gold'
+    }
+  };
+
+  // function to update records
+function updateRecords(records, id, prop, value) {
+return records;
+}
+
+
+updateRecords(recordCollection, 5439, 'artist', 'ABBA'); // testing the function
