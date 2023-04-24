f8815d6a88c7ef979a38db998101319aa24fbf82
Mizakson <maxwellizakson@gmail.com>
2023-04-24 11:30:42 -0400

    modified counting_cards.js

--- module_1/notes/sub_module_3/counting_cards.js
+++ module_1/notes/sub_module_3/counting_cards.js
@@ -1,3 +1,32 @@
 // sub module 3 final mini program
 // counting cards
 
+let count = 0;
+
+function cc(card) {
+
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count ++ ; // increment by +1
+      break;
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+      count --; // increment by -1
+      break;
+
+  }
+
+  var holdbet = 'Hold'
+  if (count > 0) {
+    holdbet = 'Bet'
+  }
+
+  return count + " " + holdbet;
+}
