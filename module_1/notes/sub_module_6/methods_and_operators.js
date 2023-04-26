375a51ec1044ca91d5a070185cc599ccc72685d8
Mizakson <maxwellizakson@gmail.com>
2023-04-26 12:44:21 -0400

    created sub_module_6 amd methods_and_operators.js

---
+++ module_1/notes/sub_module_6/methods_and_operators.js
@@ -0,0 +1,20 @@
+// notes on the final submodule in the first module "Basic Java Script"
+
+/* 
+generate random fractions (decimals) in JavaScript
+
+Math.random() function - generates a random decimal number between 0 and 1.
+Math.random can return a 0 and not a 1
+
+generate random whole numbers in JavaScript
+
+1. Math.random()
+2. multiply the generated decimal by 20 (to make the range between 1 and 9)
+3. use Math.floor, rounding the number down to its nearest whole number
+
+ex.
+
+Math.floor(Math.random() * 20);
+
+
+*/
