78f117ba6f7e2140b4c83a8d63047da0073302e0
Mizakson <maxwellizakson@gmail.com>
2023-04-26 13:23:28 -0400

    modified recursion.js

--- module_1/notes/sub_module_6/recursion.js
+++ module_1/notes/sub_module_6/recursion.js
@@ -2,5 +2,25 @@
 
 // countdown program
 
+function countdown(n) {
+    if (n < 1) {
+      return [];
+    } else {
+      const arr = countdown(n - 1); // this is where the recursion occurs
+      arr.unshift(n); // performing the array operation
+      return arr;
+    }
+  }
+
+
 
 // creating a range of numbers
+function rangeOfNumbers(startNum, endNum) {
+    if (endNum < startNum) {
+    return [];
+    } else {
+    const numbers = rangeOfNumbers(startNum, endNum - 1);
+    numbers.push(endNum);
+    return numbers;
+    }
+  };
