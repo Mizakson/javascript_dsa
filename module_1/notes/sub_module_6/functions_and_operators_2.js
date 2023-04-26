a2191a87393498761da048d77855eaf8d9d7f402
Mizakson <maxwellizakson@gmail.com>
2023-04-26 13:08:29 -0400

    modified functions_and_operators_2.js

--- module_1/notes/sub_module_6/functions_and_operators_2.js
+++ module_1/notes/sub_module_6/functions_and_operators_2.js
@@ -25,6 +25,87 @@ Math.floor(Math.random() * (max - min + 1)) + min
 
 
 parseInt function
+ - parses a string and returns an integer
 
+ ex. 
+ const a = parseInt("007");
+
+ - using a function
+ function convertToInteger(str) {
+  str = parseInt(str);
+  return str;
+}
+
+convertToInteger("56");
+
+
+parseInt with a Radix (base of each number system):
+used for binary
+
+pseudocode
+parseInt(string, radix);
+
+const a = parseInt("11", 2);
+
+
+converting binary into integer function:
+
+function convertToInteger(str) {
+  return parseInt(str, 2);
+}
+  
+convertToInteger("10011");
+
+*/
+
+
+/* 
+conditional operator:
+(aka ternary operator)
+
+a one line if-else expression
+
+syntax is a ? b : c
+
+a = condition
+b = code to run when the condition returns true
+c = code to run when the condition returns false
+
+
+ex. w/ if/else statement
+function findGreater(a, b) {
+    if (a > b) {
+        return "a is greater";
+    } else {
+        return "b is greater or equal"
+    }
+}
+
+
+now using ternary operator...
+
+function findGreater(a, b) {
+    return a > b ? "a is greater" : "b is greater or equal";
+}
+
+
+using multiple ternary operators:
+function findGreater(a, b) {
+    return (a === b) ? "a and b are equal"
+    : (a > b) ? "a is greater"
+    : "b is greater";
+}
+
+used when chaining together multiple conditions
+
+function checking the sign of an integer
+
+function checkSign(num) {
+  return (num > 0) ? "positive"
+  : (num < 0) ? "negative"
+  : "zero";
+}
+
+checkSign(10);
 
 */
