3eac047723ef067442dba0ea82d08a34484f6b1d
Mizakson <maxwellizakson@gmail.com>
2023-04-27 12:42:25 -0400

    modified let_and_var.js

--- module_2/notes/let_and_var.js
+++ module_2/notes/let_and_var.js
@@ -17,3 +17,39 @@ for (var i = 0; i < 3; i++) {
 
 console.log(numArray); // [0, 1, 2]
 console.log(i); // 3
+
+// declaring the var before the loop
+var numArray = [];
+var i;
+for (i = 0; i < 3; i++) {
+    numArray.push(i);
+}
+
+// same output
+console.log(numArray); // [0, 1, 2]
+console.log(i); // 3
+
+
+// example using let statements instead of var
+let printNumTwo;
+for (let i = 0; i < 3; i++) {
+  if (i === 2) {
+    printNumTwo = function() {
+      return i;
+    };
+  }
+}
+console.log(printNumTwo());
+console.log(i);
+
+
+// ex 2
+function checkScope() {
+    let i = 'function scope';
+    if (true) {
+      let i = 'block scope';
+      console.log('Block scope i is: ', i);
+    }
+    console.log('Function scope i is: ', i);
+    return i;
+  }
