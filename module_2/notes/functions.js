1bbd62cf2c521227f4f014bcf4cbd4bfff8c390f
Mizakson <maxwellizakson@gmail.com>
2023-04-28 09:47:13 -0400

    modified functions.js

--- module_2/notes/functions.js
+++ module_2/notes/functions.js
@@ -3,7 +3,58 @@
 /* 
 arrow functions:
 
+it isn't optimal to name functions under certain circumstances.
+the most common being when passing another function as an argument
+to another function.
 
+these types of functions are called inline functions.
+they are concise anonymous functions. 
+an arrow function uses a new syntax to write inline functions
+
+pseudocode:
+const arrowFuncName = () => {
+    function code here
+}
+
+
+ex 1.
+const magic = () => {
+    return newValue();
+}
+
+ex 2.
+const myFunc = () => "value";
+// returns the string value by default
+
+
+---
+---
+
+writing arrow functions with parameters:
+
+put a parameter inside of the parentheses
+
+const doubler = (item) => item * 2;
+// item is the parameter
+
+doubler(4); //calling the function
+
+if there is only one parameter parentheses can be omitted
+like so...
+
+const doubler = item = item * 2;
+
+
+now with more than one parameter...
+
+const multiplier = (item, multi) => item * multi;
+multiplier(4, 2);
+
+
+*/
+
+/* 
+setting default parameters for functions
 
 
 */
