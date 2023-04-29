e610a0bcca73d585e05a454d093f7e78f7a50e49
Mizakson <maxwellizakson@gmail.com>
2023-04-29 11:56:27 -0400

    created literals.js

---
+++ module_2/notes/literals.js
@@ -0,0 +1,23 @@
+// notes on literals
+
+/* 
+creating strings using template literals:
+
+template literal - a special type of string that 
+makes creating complex strings easier
+
+ex.
+const person = {
+  name: "Zodiac Hasbro",
+  age: 56
+};
+
+const greeting = `Hello, my name is ${person.name}!
+I am ${person.age} years old.`;
+
+console.log(greeting);
+
+
+
+
+*/
