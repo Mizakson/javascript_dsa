04420528f5ad128787d413b2ec897d6cb26bb7e9
Mizakson <maxwellizakson@gmail.com>
2023-05-01 10:08:17 -0400

    modified declaritive_functions.js

--- module_2/notes/declaritive_functions.js
+++ module_2/notes/declaritive_functions.js
@@ -3,7 +3,24 @@
 /* 
 declarative functions with ES6:
 
+it is possible to declare functions without using the function keyword
 
+ex.
+// code with function keyword
+const person = {
+    name: "Max",
+    sayHello: function() {
+        return `Hello! My name is ${this.name}.`;
+    }
+};
+
+// new code without function keyword
+const person = {
+    name: "Max",
+    sayHello() {
+        return `Hello! My name is ${this.name}.`;
+    }
+};
 
 
 */
