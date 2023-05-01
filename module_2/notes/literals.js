1b9177207ae8b3710617aad5eb89fd5165c2ca76
Mizakson <maxwellizakson@gmail.com>
2023-05-01 08:46:04 -0400

    modified literals.js

--- module_2/notes/literals.js
+++ module_2/notes/literals.js
@@ -17,7 +17,32 @@ I am ${person.age} years old.`;
 
 console.log(greeting);
 
+ex.
+const result = {
+  success: ["max-length", "no-amd", "prefer-arrow-functions"],
+  failure: ["no-var", "var-on-top", "linebreak"],
+  skipped: ["no-extra-semi", "no-dup-keys"]
+};
 
+function makeList(arr) {
+const failureItems = [];
+  for (let i = 0; i < arr.length; i++) {
+    failureItems.push(`<li class="text-warning">${arr[i]}</li>`);
+  }
+
+
+*/
+
+
+/* 
+property shorthand:
+
+const getMousePosition = (x, y) => ({
+  x: x,
+  y: y
+});
+
+const getMousePosition = (x, y) => ({ x, y });
 
 
 */
