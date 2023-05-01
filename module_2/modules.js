5ad2101035a9a8d4e856aefc766c98bb6c04c182
Mizakson <maxwellizakson@gmail.com>
2023-05-01 11:57:04 -0400

    modified modules.js

---
+++ module_2/modules.js
@@ -0,0 +1,10 @@
+// notes on modules
+
+/* 
+in order to modularize JavaScript code you need
+to create a script in your HTML document with the type = module
+
+pseudocode:
+<script type="module" src="filename.js"></script>
+
+*/
