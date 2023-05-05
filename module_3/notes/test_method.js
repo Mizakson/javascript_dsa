2776b08a04c47a75c4210c4d6c417017e0d31455
Mizakson <maxwellizakson@gmail.com>
2023-05-05 12:04:03 -0400

    modified test_method.js

--- module_3/notes/test_method.js
+++ module_3/notes/test_method.js
@@ -53,3 +53,59 @@ pseudocode...
 put the flag at the end of the second backslash
 
 */
+
+/* 
+lookaheads:
+patterns that tell JavaScript to look-ahead in the string to
+check for patterns further along, this is useful when you want
+to search for multiple patterns over the same string
+
+there are two types of lookaheads, positive and negative.
+
+positive
+- will look to make sure the element in the search pattern is there,
+but won't actually match it
+- used as (?=...), the (...) part is the required part that is not matched
+
+negative
+- will look to make sure the element in the search pattern isn't there
+- used as (?!...), (...) is the pattern that you don't want to be there
+- the rest of the pattern is returned if the negative lookahead part 
+is not present 
+
+
+*/
+
+// example of lookaheads
+let sampleWord = "skyscraper";
+let pwRegex =  /(?=\w{6})(?=\w*\d{2})/;
+let result = pwRegex.test(sampleWord);
+
+/* 
+the constraints are greater than 5 chars
+and at least 2 consecutive digits
+
+1st lookahead: (?=\w{6})
+- greater than 5 chars
+
+2nd lookahead: (?=w*\d{2})
+- at least 2 consecutive digits
+
+*/
+
+/* 
+checking for mixed grouping of characters:
+
+you can check for groups of characters using parentheses ()
+and the or (|) operand
+
+example
+
+want to check for either 'sky' or 'story' in a string
+
+let testStr = 'story';
+lest testRegex = /s(k|tor)y/;
+testRegex.test(testStr);
+
+*/
+
