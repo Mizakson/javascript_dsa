7ddf61d3ed08d9947600bb3e0d696967922f8e67
Mizakson <maxwellizakson@gmail.com>
2023-05-08 14:36:54 -0400

    modified arrays.js

--- module_5/notes/arrays.js
+++ module_5/notes/arrays.js
@@ -5,7 +5,59 @@
 
 /* 
 arrays:
+used to store a collection of data
+
+the simplest implementation is a one-dimensional array
+holds all data types
 
 
+arrays have a length property, which can be accessed
+using arrayName.length
+
+arrays are capable of storing complex objects,
+a.k.a multi-dimensional arrays
+
+myArray = [1, 'two', true];
+
+
+-------
+-------
+
+
+accessing array contents using bracket notation:
+a fundamental feature of any data structure is the ability
+to retrieve specific data on command
+
+use bracket notation [] for indexing
+
+
+-------
+-------
+
+
+add items to an array using push() and unshift()
+
+push() adds an element to the end of an array
+unshift() adds an element to beginning of an array
+
+
+remove items from an array using pop() and shift()
+
+pop() removes an element from the end of an array
+shift() removes an element from the beginning of an array
+
+
+remove specific items from an array using splice()
+
+splice can take up to 3 parameters,
+
+1st parameter represents the index on the array to begin removing elements
+2nd parameter represents the number of elements to delete
+
+
+add specific items using splice() 
+
+the third parameter in splice is used to add to the array
+takes on or more elements
 
 */
