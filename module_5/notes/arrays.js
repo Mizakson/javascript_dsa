9c1552f1d11dfd680e86d44350d0555d7baa88f6
Mizakson <maxwellizakson@gmail.com>
2023-05-10 10:24:28 -0400

    modified arrays.js

--- module_5/notes/arrays.js
+++ module_5/notes/arrays.js
@@ -79,6 +79,7 @@ weatherConditions would still have ['rain', 'snow', 'sleet', 'hail', 'clear']
 -------
 -------
 
+
 copying an array with the spread() operator
 - slice allows elements of an array to be copied
 - spread allows arrays themselves to be copied
@@ -101,6 +102,69 @@ let arr1 = [3, 4];
 let arr2 = [1, 2, ...arr1, 5, 6];
 
 
+-------
+-------
+
+
+indexOf() method
+
+used to check for the presence of an element in an array
+takes the element as a parameter
+
+when called it returns the index of the element in the array,
+or -1 if the element doesn't exist in the array 
+
+
+-------
+-------
+
+
+iterating through all of an array's items using a for loop
+
+
+ex.
+want to iterate through an array of integers,
+if the element is greater than 10 it is added to a new array
+
+function greaterThanTen(arr) {
+    let newArr =[];
+    for (let i=0; i < arr.length; i++) {
+        if (arr[i] > 10) {
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
+}
+
+// call the function
+console.log(greaterThanTen([1, 11, 1111, 22, 2, 2222, 3, 33, 333]));
+// only adds numbers greater than 10 to the newArr
+
+
+-------
+-------
+
+
+multi-dimensional arrays:
+
+arrays can become more complex when you nest arrays inside of arrays,
+these complex arrays are called multi-dimensional arrays.
+
+multi-dimensional arrays can have infinite levels,
+when indexing multi-dimensional arrays, use bracket notation chained to each other
+
+ex.
+
+arr[1][2][3][4][5]
+
+1st bracket represents the 1st level, 2nd == 2nd level and so on...
+
+ex.
+5 level nest array
+let myNestedArray = [
+  1, 
+  [2, ['deep', 3, ['deeper', 4, ['deepest', 5]]]]
+];
 
 
 */
