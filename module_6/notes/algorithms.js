7d860805076f7a39a2ea8005801aaf133fa8bedb
Mizakson <maxwellizakson@gmail.com>
2023-05-10 12:23:54 -0400

    modified algorithms.js

---
+++ module_6/notes/algorithms.js
@@ -0,0 +1,16 @@
+// notes on algorithms
+
+// -----------------------------
+
+/* 
+
+Algorithm:
+
+- a series of step-by-step instructions that describe how
+to do something
+
+to write an effective algorithm, when trying to solve a problem,
+break it down into smaller parts and how these smaller parts can be 
+solved with code
+
+*/
