971f09d43603470598c1ed82ca1183bc639fe0bb
Mizakson <maxwellizakson@gmail.com>
2023-05-10 12:58:26 -0400

    modified algorithms.js

--- module_6/notes/algorithms.js
+++ module_6/notes/algorithms.js
@@ -33,9 +33,37 @@ function convertCtoF(celsius) {
 
 
 // algorithm to reverse a string
+function reverseString(str) {
+    str = str.split(''); // split string into individual letters
+    str = str.reverse(''); // reverse the letters
+    str = str.join(''); // concatenate the letters
+    return str;
+  }
+  
+  reverseString("hello");
 
 
+// -----------------------------
 
 
+// algorithm to factorialize a number
+function factorialize(num) {
+    let product = 1;
+    for (let i = 2; i <= num; i++) {
+      product *= i;
+    }
+    
+    return product;
+  }
+  
+// exaplanation
 
+/* 
+for every number greater than 1 passed through the function,
+the for loop will be executed.
+
+for loop:
+increments by one and re-calculates the product to the value of num
+
+*/
 
