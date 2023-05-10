804c65f52226527164af50553c97c460bb1e5e9e
Mizakson <maxwellizakson@gmail.com>
2023-05-10 12:27:47 -0400

    modified algorithms.js

--- module_6/notes/celsius_to_farenheit.js
+++
@@ -1,9 +0,0 @@
-// algorithm that converts celsius to farenheit
-// (c * (9/5)) + 32 = f
-
-function convertCtoF(celsius) {
-    let fahrenheit = (celsius * (9 / 5) + 32);
-    return fahrenheit;
-  }
-  
-  convertCtoF(30);
