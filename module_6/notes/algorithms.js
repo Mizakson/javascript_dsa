0249522ee72d7c43bb4d8c0eed581469c987ff42
Mizakson <maxwellizakson@gmail.com>
2023-05-11 13:52:31 -0400

    modified algorithms.js

--- module_6/notes/algorithms.js
+++ module_6/notes/algorithms.js
@@ -67,3 +67,86 @@ increments by one and re-calculates the product to the value of num
 
 */
 
+
+// -----------------------------
+
+
+// algorithm to find the longest word in a string
+
+function findLongestWordLength(str) {
+  let words = str.split(' '); // words == each word in the string
+  let maxLen = 0; // set max length
+  
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > maxLen) {
+      maxLen = words[i].length;
+    }
+  }
+
+/* 
+for loop iterates through each word in the split array
+if the length of the word being iterated is greater than the previous
+length, the max length is replaced.
+
+this is continued until the end of the loop
+
+*/
+
+  return maxLen;
+}
+
+
+// -----------------------------
+
+
+// algorithm to return the largest numbers in an array with 4 sub-arrays
+
+
+function largestOfFour(arr) {
+  let results = []; 
+
+  // for loop to iterate through the array
+    for (let i = 0; i < arr.length; i++) {
+      let max = arr[i][0];
+      for (let j = 1; j < arr[i].length; j++) {
+        if (arr[i][j] > max) {
+          max = arr[i][j];
+        }
+      }
+      results[i] = max;
+    }
+
+  return results;
+}
+
+
+// -----------------------------
+
+
+// algorithm that is the same as the .endsWith() method
+
+
+function confirmEnding(str, target) {
+  if (str.slice(str.length - target.length) === target) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+// -----------------------------
+
+
+// algorithm that is the same as the .repeat() method
+
+function repeatStringNumTimes(str, num) {
+  let totalStr = ""; // declare var for the output
+
+  for (let i = 0; i < num; i++) {
+    totalStr += str; // for loop that repeats the string until the loop reaches the num
+  }
+  
+  return totalStr;
+}
+
