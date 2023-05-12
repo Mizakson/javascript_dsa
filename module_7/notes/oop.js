1d9bd0cc01c00a45321ece8a2be9f48b3003391e
Mizakson <maxwellizakson@gmail.com>
2023-05-12 15:06:07 -0400

    modified oop.js

--- module_7/notes/oop.js
+++ module_7/notes/oop.js
@@ -1,9 +1,47 @@
 // notes on OOP (Object Oriented Programming)
 
+// -----------------------------
+
 /* 
 
 OOP:
-
-
+an approach to software development,
+using objects and classes to organize code and 
+to describe things and what they do
 
 */
+
+// -----------------------------
+
+/* 
+objects in JavaScript are used to model real-world objects,
+giving them properties and behavior like physical objects 
+
+
+*note
+when calling an object by a specific property use this pseudocode
+
+console.log(objName.propName);
+
+*/
+
+// -----------------------------
+
+/* 
+objects can have a special type of property called a method,
+methods are properties that are functions
+
+pseudocode
+
+let obj1 = {
+    prop1: val1,
+    prop2: function() {
+        function code here
+    },
+}
+
+obj1.prop2()
+
+*/
+
+// -----------------------------
