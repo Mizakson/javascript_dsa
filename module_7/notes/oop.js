17d7f682509415ab10aa1f6610f5ae8329a8d841
Mizakson <maxwellizakson@gmail.com>
2023-05-13 18:55:21 -0400

    modified oop.js

--- module_7/notes/oop.js
+++ module_7/notes/oop.js
@@ -42,6 +42,105 @@ let obj1 = {
 
 obj1.prop2()
 
+
+========
+========
+
+'this' keyword is preferrable when writing return statements in methods
+
+ex.
+
+let obj1 = {
+    name: 'Max',
+    sayName: function() {return 'My name is ' + this.name + '.'}
+}
+
+instead of typing the obj name, the this keyword makes code more resuable
+
 */
 
 // -----------------------------
+
+/* 
+constructors:
+
+functions that create new objects,
+they define properties and behaviors that will belong to the new object
+
+(constructors are basically blueprints for the creation of new objects)
+
+ex.
+
+function SoccerTeam() {
+    this.name = "Liverpool";
+    this.country = "England";
+    this.league = "Premier League"
+}
+
+more notes on constructors...
+
+*** 1. constructors are defined with a capitalized name
+to distinguish them from non-constructor functions
+2. constructors use the this keyword to set the properties of the new object
+(this refers to the new object)
+3. constructors define properties and behaviors instead of
+returning a value like some other function
+
+
+------
+------
+
+use the 'new' operator when calling a constructor.
+
+ex. 
+let team = new SoccerTeam();
+
+the new operator tells JavaScript to create a new instance of SoccerTeam
+called team.
+
+object's created using constructors can also have its properties be accessed
+and modified.
+
+------
+------
+
+constructors can be extended to recieve arguments.
+
+instead of putting a specific value for the properties 
+in the constructor definition, use parameters and then pass
+in values as arguments to define each unique instance of the object.
+
+using the SoccerTeam example from above...
+
+function SoccerTeam(name, country, league) {
+    this.name = name;
+    this.country = country;
+    this.league = league;
+}
+
+if i wanted create a new object for a different team...
+
+let manCity = new SoccerTeam("Manchester City", "England", "Premier League");
+*** don't forget to use the 'new' keyword
+
+
+the variable manCity is a specific instance of the SoccerTeam object.
+
+------
+------
+
+anytime constructor function creates a new object, that object
+is called an instance of its constructor. you can verify if an object
+was created with a specific constructor using instanceof.
+returns a boolean.
+
+ex.
+manCity instance of SoccerTeam;
+
+------
+------
+
+
+
+
+*/
